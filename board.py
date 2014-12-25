3a4edb4fc4fc461024425e61f1fdfcd7f0d4a486
Mark Smith <smith.h.mark@gmail.com>
2014-12-24 19:38:48 -0600

    adding a func to produce an illegal board for testing

--- board.py
+++ board.py
@@ -1,6 +1,30 @@
 # board.py
 
 
+HEIGHT = 9
+WIDTH = 9
+LENGTH = HEIGHT * WIDTH
+
 def empty():
-    return [0] * (9*9)
-    
+    return [0] * (HEIGHT * WIDTH)
+
+def illegal():
+    brd = empty()
+    for idx, unused_sqr in enumerate(brd):
+        brd[idx] = idx
+    return brd
+
+def row(brd, ii):
+    # index = rownum * rowlen + colnum
+    start_idx = ii * WIDTH
+    return brd[start_idx:(start_idx + WIDTH)]
+
+def column(row,ii):
+    pass
+
+def validate(brd):
+    down = [set() for unused in range(9)]
+    across = [set() for unused in range(9)]
+    squares = [set() for unused in range(9)]
+
+
--- test_board.py
+++ test_board.py
@@ -6,3 +6,10 @@ def test_empty():
     assert len(brd) == 9*9
     for square in brd:
         assert square == 0
+
+def test_illegal():
+    brd = board.illegal()
+
+    print(brd)
+    for ii in range(len(brd)):
+        assert brd[ii] == ii
